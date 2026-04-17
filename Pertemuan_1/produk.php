7cbc878e88e1f1c0d082eed8da4a298d10904650
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-04-17 09:25:36 +0700

    Feat: Membuat method untuk mencetak label produk dari nilai property class

--- Pertemuan_1/produk.php
+++ Pertemuan_1/produk.php
@@ -1,8 +1,7 @@
 <?php
 
-//Jualan produk
-//Komik
-//Game
+// Jualan produk
+// Manga
 
 class produk {
 
@@ -12,6 +11,10 @@ public $judul = "judul",
        $penerbit = "penerbit",
        $harga = 0;
 
+       public function cetakLabelProduk() {
+        // menggunakan $this untuk mengakses property dalam class
+        return "Manga : $this->judul, $this->penulis, $this->penerbit, Harga: $this->harga";
+       }
 
 }
 
@@ -23,20 +26,4 @@ $produk1->penerbit = "Shueisha";
 $produk1->harga = 30000;
 
 // menampilkan informasi produk
-echo "Komik : $produk1->judul, $produk1->penulis, $produk1->penerbit, Harga: $produk1->harga";
-
-echo "<br>";
-
-// membuat objek produk kedua
-$produk2 = new produk();
-$produk2->judul = "Call of Duty";
-$produk2->developer = "Activision";
-$produk2->harga = 500000;
-// menampilkan informasi produk
-echo "Game : $produk2->judul, $produk2->developer, Harga: $produk2->harga";
-
-echo "<br>";
-
-var_dump($produk1);
-echo "<br>";
-var_dump($produk2);
+echo $produk1->cetakLabelProduk();

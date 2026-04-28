1f189d4489e0f79568df8589af01201fab55b5a4
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-04-28 09:51:52 +0700

    Feat: Mengubah property judul menjadi private sehingga jika ingin melakukan aksi terhadap judul seperti mengubah atau menampilkan property judul diluar class (termasuk child) harus memanggil/menggunakan method setter atau getter judul

--- produk.php
+++ produk.php
@@ -7,7 +7,7 @@
 class Produk {
 
 // property
-public $judul;
+private $judul;
 // Protected : hanya bisa diakses dalam class ini dan class turunannya
 protected $harga;
 
@@ -18,8 +18,18 @@ protected $harga;
         $this->judul = $judul;
         $this->harga = $harga;
        }
-        // method untuk mencetak informasi harga produk
-       protected function cetakHarga() {
+
+    public function setJudul($judul) {
+        echo "Warning: Judul diubah dari " . $this->judul . " menjadi: " . $judul . "<br>";
+        $this->judul = $judul;
+    }
+
+    public function getJudul() {
+        return $this->judul;
+    }
+
+    // method untuk mencetak informasi harga produk
+    protected function cetakHarga() {
         // Mengembalikan string yang berisi harga produk dengan format yang sesuai
         return "Harga: Rp. " . number_format($this->harga, 0, ',', '.');
        }
@@ -39,7 +49,7 @@ class Komik extends Produk {
         }
 
     public function cetakInfoProduk() {
-        return "Komik: $this->judul | Penulis: $this->penulis | Penerbit: $this->penerbit | " . parent::cetakHarga(); 
+        return "Komik: " . parent::getJudul() . " | Penulis: " . $this->penulis . " | Penerbit: " . $this->penerbit . " | " . parent::cetakHarga();
     }
 }
 
@@ -55,14 +65,20 @@ class Game extends Produk {
 
     public function cetakInfoProduk() {
         // (Work) method cetakHarga() sudah bisa diakses karena sudah diubah menjadi protected
-        return "Game: $this->judul | Developer: $this->developer | " . parent::cetakHarga();
+        return "Game: " . parent::getJudul() . " | Developer: " . $this->developer . " | " . parent::cetakHarga();
     }
 }
 
 $produk1 = new Komik("Attack on Titan", "Hajime Isayama", "Shueisha", 300000);
 $produk2 = new Game("Call of Duty", "Activision", 2500000);
 
-// menampilkan informasi produk
+// Mengganti judul produk menggunakan setter
+$produk1->setJudul("Shingeki no Kyojin");
+// Menampilkan informasi produk menggunakan getter dan method cetakInfoProduk
 echo $produk1->cetakInfoProduk();
 echo "<br>";
+echo "<hr>";
+// Mengganti judul produk menggunakan setter
+$produk2->setJudul("Call of Duty: Modern Warfare");
+// Menampilkan informasi produk menggunakan getter dan method cetakInfoProduk
 echo $produk2->cetakInfoProduk();

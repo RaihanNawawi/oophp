fcf77a19694acfac604a145690f3114d58112231
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-05-05 09:44:45 +0700

    Feat: Menggunakan require untuk memanggil semua class didalam folder Produk secara manual

--- autoloading/index.php
+++ autoloading/index.php
@@ -1,5 +1,10 @@
 <?php
 
+// Menggunakan require untuk memanggil semua class didalam folder Produk
+require_once 'App/Produk/InfoProduk.php';
+require_once 'App/Produk/Produk.php';
+require_once 'App/Produk/Komik.php';
+require_once 'App/Produk/Game.php';
 
 $produk1 = new Komik("Attack on Titan", "Hajime Isayama", "Shueisha", 300000);
 $produk2 = new Game("Call of Duty", "Activision", 2500000);

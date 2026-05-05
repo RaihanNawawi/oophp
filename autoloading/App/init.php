58ceabb808398bd1205a6488033c0fead255e2b6
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-05-05 09:58:43 +0700

    Feat: Menggunakan function autoload untuk memanggil class yang ada didalam folder Produk secara otomatis

--- autoloading/App/init.php
+++ autoloading/App/init.php
@@ -1,7 +1,5 @@
 <?php
-
-// Menggunakan require untuk memanggil semua class didalam folder Produk
-require_once 'Produk/InfoProduk.php';
-require_once 'Produk/Produk.php';
-require_once 'Produk/Komik.php';
-require_once 'Produk/Game.php';
+// Menggunakan autoload untuk memanggil class yang ada didalam folder Produk secara otomatis
+spl_autoload_register(function($class) {
+    require_once __DIR__ . '/Produk/' . $class . '.php';
+});

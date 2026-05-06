e5b1426fa83eabede527a20090f5326eaa9cbf3f
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-05-06 10:50:35 +0700

    Feat: (Solution) Studi kasus penyelesaian problem sebelumnya dengan 2 solusi yakni memperbaiki urutan deklarasinya atau menggunakan autoload (best practice)

--- autoloading/App/init.php
+++ autoloading/App/init.php
@@ -1,9 +1,14 @@
 <?php
 
 //  class yang mengimplementasi interface InfoProduk harus deklarasikan setelah interface InfoProduk dideklarasikan
-//  Ini akan error karena Class Coba mengimplementasi interface InfoProduk, sementara interface InfoProduk belum dideklarasikan
-require_once 'Produk/Produk.php'; // Class
-require_once 'Produk/Komik.php'; // Class
-require_once 'Produk/Game.php'; // Class
-require_once 'Produk/InfoProduk.php'; // Interface
+//  Solusi  1 (Old Approach): Mendeklarasikan interface InfoProduk terlebih dahulu, baru setelah itu mendeklarasikan class yang mengimplementasi interface tersebut
+// require_once 'Produk/InfoProduk.php'; // Interface
+// require_once 'Produk/Produk.php'; // Class
+// require_once 'Produk/Komik.php'; // Class
+// require_once 'Produk/Game.php'; // Class
 
+// Solusi 2 (Best Practice): Menggunakan autoloading, sehingga kita tidak perlu lagi memanggil file-file class secara manual
+// function autoloading untuk memanggil file class secara otomatis
+spl_autoload_register(function ($class) {
+    require_once __DIR__ . '/Produk/' . $class . '.php';
+});

f47e194e7edce5d01902bf9a56fcc52167321da9
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-05-06 11:57:07 +0700

    Feat: Studi kasus penggunaan alias untuk mempermudah intansiasi (deklarasi/penulisan) class yang menggunakan namespace

--- namespace/index.php
+++ namespace/index.php
@@ -1,8 +1,10 @@
 <?php
+use App\Produk\User as ProdukUser; // Menggunakan alias untuk class User yang berada di folder Produk
+use App\Services\User as ServicesUser; // Menggunakan alias untuk class User yang berada di folder Services
 
 // Memanggul file init.php yang berada di folder Produk
 require_once 'App/init.php';
 
-new \App\Produk\User(); // Memanggil class User yang berada di folder Produk
+new ProdukUser(); // Memanggil class User yang berada di folder Produk
 
-new \App\Services\User(); // Memanggil class User yang berada di folder Services
+new ServicesUser(); // Memanggil class User yang berada di folder Services

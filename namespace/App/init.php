9f639d402cfba13f932cc8448d7e64fe4ab9b661
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-05-06 11:25:18 +0700

    Feat: (Problem) Studi Kasus menggunakan autoload pada saat kita menggunakan namespace untuk class yang telah kita buat, nama class (User) -> nama namespace (App\Produk\User)

--- namespace/App/init.php
+++ namespace/App/init.php
@@ -1,4 +1,9 @@
 <?php
 
-require_once 'Services/User.php';
-require_once 'Produk/User.php';
+spl_autoload_register(function($class) {
+    require_once __DIR__ . '/Produk/' . $class . '.php';
+});
+
+spl_autoload_register(function($class) {
+    require_once __DIR__ . '/Services/' . $class . '.php';
+});

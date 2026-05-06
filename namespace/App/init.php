18d1348b8c7346a864a6f165c2debe1d0d87ed36
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-05-06 11:51:59 +0700

    Feat: (Solution) menggunakan explode() untuk memecah namespace menjadi array, dan end() akan mengambil nilai terakhir dari array yang dimana adalah nama class itu sendiri

--- namespace/App/init.php
+++ namespace/App/init.php
@@ -1,9 +1,16 @@
 <?php
 
+
 spl_autoload_register(function($class) {
+    // $class = App\Produk\User dengan menggunakan explode() maka akan diubah kedalam array ['App', 'Produk', 'User']
+    $class = explode('\\', $class);
+    $class = end($class); 
     require_once __DIR__ . '/Produk/' . $class . '.php';
 });
 
 spl_autoload_register(function($class) {
+    // $class = App\Services\User dengan menggunakan explode() maka akan diubah kedalam array ['App', 'Services', 'User']
+    $class = explode('\\', $class);
+    $class = end($class);
     require_once __DIR__ . '/Services/' . $class . '.php';
 });
